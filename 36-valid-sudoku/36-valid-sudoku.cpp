d793ebac34fdff406549bfa8e085219ce8c2bf5f
radhikavarshney <93032468+radhikavarshney@users.noreply.github.com>
2022-09-26 00:21:41 +0530

    Time: 56 ms (18.89%), Space: 20.4 MB (21.68%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<set<int>> rows(9), cols(9), blocks(9);
-        
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                
-                if (board[i][j] == '.') continue;
-                
-                int curr = board[i][j];
-                if (rows[i].count(curr) || cols[j].count(curr) || blocks[(i/3)*3+j/3].count(curr)) 
-                    return false;
-                
-                rows[i].insert(curr);
-                cols[j].insert(curr);
-                blocks[(i/3)*3+j/3].insert(curr);
+        vector<set<int>> r(9), c(9), b(9);
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0; j<9;j++)
+            {
+                if(board[i][j]!='.')
+                {
+                    int now=board[i][j];
+                    int var=j/3+(i/3)*3;
+                    if(r[i].count(now)||c[j].count(now)||b[var].count(now))
+                    {
+                        return false;
+                    }
+                    r[i].insert(now);
+                    c[j].insert(now);
+                    b[var].insert(now);
+                }
             }
         }
-        
-        return true;
+       return true; 
     }
 };
