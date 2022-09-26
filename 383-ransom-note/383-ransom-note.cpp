145b667881fd92e4868d761e22fc3f3a232120ec
radhikavarshney <93032468+radhikavarshney@users.noreply.github.com>
2022-09-26 21:42:38 +0530

    Time: 61 ms (6.85%), Space: 9.8 MB (5.36%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char, int> ansr;
-        unordered_map<char, int> ansm;
         for(char& c: ransomNote)
         {
             ansr[c]++;
